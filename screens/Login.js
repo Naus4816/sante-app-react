bb382cf0a93d97539f08bb645daff3f720fbab40
Naus4816 <128583047+Naus4816@users.noreply.github.com>
2024-01-25 23:50:07 +0100

    Update Login.js

--- screens/Login.js
+++ screens/Login.js
@@ -1,135 +1,179 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-import axios from 'axios';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
+import AsyncStorage  from '@react-native-async-storage/async-storage';
 
-const LoginForm = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [ userId,setUserId ] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-    const handleSubmit = async () => {
-        try {
-            const response = await axios.post('URL_DE_VOTRE_API', { email, password });
-        } catch (error) {
+
+  const storeUserId = async (userId) => {
+    try {
+      await AsyncStorage.setItem('userId', userId.toString());
+    } catch (error) {
+      console.error('Erreur lors du stockage de l\'ID de l\'utilisateur:', error);
+    }
+  };
+    
+  const handleLogin = async () => {
+    try {
+      const response = await fetch('http://172.20.10.2:8888/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+      const data = await response.json();
+      
+      setUserId(data.id); 
+
+      if (response.status === 200) {
+        // Vérifier si l'utilisateur est un administrateur
+        if (data.is_admin) {
+          // Naviguer vers le tableau de bord administrateur
+          navigation.navigate('AdminDashboard');
+        } else {
+          // Naviguer vers le tableau de bord utilisateur standard
+          storeUserId(userId);
+          navigation.navigate('UserDashboard');
         }
-    };
-
-    return (
-        <View style={styles.container}>
-            <View style={styles.card}>
-                <View style={styles.cardHeader}>
-                    <Text style={styles.headerTitle}>Connectez-vous à votre compte</Text>
-                </View>
-                <View style={styles.cardBody}>
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={setEmail}
-                        value={email}
-                        placeholder="Email"
-                        keyboardType="email-address"
-                        autoCapitalize="none"
-                    />
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={setPassword}
-                        value={password}
-                        placeholder="Mot de passe"
-                        secureTextEntry={true}
-                        autoCapitalize="none"
-                    />
-                    <TouchableOpacity style={styles.buttonPrimary} onPress={handleSubmit}>
-                        <Text style={styles.buttonText}>Se connecter</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => { /* Logique pour mot de passe oublié */ }}>
-                        <Text style={styles.forgotPasswordLink}>Mot de passe oublié ?</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-            <Text style={styles.welcomeTitle}>Bienvenue sur Santé-App</Text>
-            <Text style={styles.welcomeText}>Rejoignez-nous et découvrez le moyen premium de vérifier votre état de santé, de gérer vos rendez-vous et de rester au courant de vos besoins médicaux en toute simplicité.</Text>
-            <TouchableOpacity onPress={() => {/* Navigation ou action */}}>
-                <Text style={styles.registerLink}>Inscrivez-vous maintenant</Text>
-            </TouchableOpacity>
+      } else {
+        // Si le statut n'est pas 200, on suppose qu'il y a une erreur de connexion
+        setErrorMessage(data.message || 'Erreur de connexion');
+      }
+    } catch (error) {
+      // Cette partie attrape les erreurs de réseau ou les erreurs survenues lors de la requête
+      setErrorMessage(error.message || 'Une erreur est survenue lors de la connexion');
+    }
+  };
+    
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.loginContainer}>
+        <View style={styles.loginInfo}>
+          {/* ... contenu de loginInfo ... */}
         </View>
-    );
+  
+        <View style={styles.loginForm}>
+          <View style={styles.card}>
+            <View style={styles.cardHeader}>
+              <Text style={styles.cardHeaderText}>Connectez-vous à votre compte</Text>
+            </View>
+  
+            <View style={styles.cardBody}>
+              {/* Affichage du message d'erreur */}
+              {errorMessage ? <Text style={styles.errorMessage}>{errorMessage}</Text> : null}
+  
+              {/* Champs de saisie Email et Mot de passe */}
+              <TextInput
+                style={styles.input}
+                onChangeText={setEmail}
+                value={email}
+                placeholder="Email"
+                keyboardType="email-address"
+              />
+              <TextInput
+                style={styles.input}
+                onChangeText={setPassword}
+                value={password}
+                placeholder="Mot de passe"
+                secureTextEntry
+              />
+  
+              {/* Bouton de connexion */}
+              <TouchableOpacity style={styles.button} onPress={handleLogin}>
+                <Text style={styles.buttonText}>Se connecter</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
+  );  
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'flex-start', // Align content to the top
-        alignItems: 'center',
-        backgroundColor: '#fff',
-        paddingTop: 30, // Add padding at the top of the screen to push the content down
-    },
-    card: {
-        borderRadius: 20,
-        elevation: 5,
-        backgroundColor: '#fff',
-        width: 500, // Adjust the width to match the first image
-        alignSelf: 'flex-end',
-        marginRight: 60,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.1,
-        shadowRadius: 20,
-        overflow: 'hidden',
-        marginTop: 20, // Add top margin if needed
-    },
-    cardHeader: {
-        backgroundColor: '#3490dc',
-        width: '100%',
-        padding: 20,
-        borderTopLeftRadius: 20, 
-        borderTopRightRadius: 20,
-    },
-    headerTitle: {
-        color: '#fff',
-        fontSize: 20,
-        fontWeight: 'bold',
-        textAlign: 'center',
-    },
-    cardBody: {
-        padding: 20,
-        width: '100%', 
-    },
-    welcomeTitle: {
-        fontSize: 24,
-        alignSelf: 'flex-start',
-        fontWeight: 'bold',
-        marginBottom: 10,
-        textAlign: 'center',
-    },
-    welcomeText: {
-        fontSize: 16,
-        alignSelf: 'flex-start',
-        textAlign: 'center',
-        marginBottom: 20,
-    },
-    registerLink: {
-        color: '#3490dc',
-        fontSize: 16,
-        textDecorationLine: 'underline',
-    },
-    input: {
-        borderRadius: 10,
-        borderWidth: 1,
-        borderColor: '#ced4da',
-        width: '100%', 
-        marginBottom: 20,
-        padding: 15,
-    },
-    buttonPrimary: {
-        backgroundColor: '#3490dc',
-        borderRadius: 20,
-        padding: 15,
-        width: '100%', 
-        alignItems: 'center', 
-    },
-    buttonText: {
-        color: '#fff',
-        fontSize: 16,
-    },
-    });
-    
-    
-export default LoginForm;
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 10, // Réduction du padding pour les petits écrans
+  },loginContainer: {
+    flexDirection: 'column', // Utilisation d'un stack layout sur mobile
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loginInfo: {
+    margin: 10, // Réduction de la marge
+    flex: 1,
+    alignItems: 'center', // Centrage du texte sur mobile
+  },
+  loginInfoTitle: {
+    fontSize: 20, // Réduction de la taille de la police pour les petits écrans
+    fontWeight: 'bold',
+    marginBottom: 10, // Réduction de l'espacement
+  },
+  loginInfoText: {
+    fontSize: 14, // Taille de police adaptée pour mobile
+    marginBottom: 10,
+    textAlign: 'center', // Centrage du texte sur mobile
+  },
+  linkText: {
+    color: '#3490dc',
+    textDecorationLine: 'underline',
+  },
+  loginForm: {
+    flex: 1,
+    width: '100%',
+  },
+  card: {
+    borderRadius: 15, // Réduction du rayon de bordure pour un look plus adapté au mobile
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    overflow: 'hidden',
+    margin: 10, // Ajout d'une marge autour de la carte
+  },
+  cardHeader: {
+    backgroundColor: '#6AC8FF',
+    padding: 15, // Réduction du padding
+  },
+  cardHeaderText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 18, // Réduction de la taille de la police pour les petits écrans
+  },
+  cardBody: {
+    backgroundColor: '#fff',
+    padding: 20, // Réduction du padding pour les petits écrans
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ced4da',
+    borderRadius: 10,
+    padding: 12, // Réduction du padding
+    marginBottom: 15,
+    fontSize: 14, // Réduction de la taille de la police
+  },
+  button: {
+    backgroundColor: '#3490dc',
+    borderRadius: 20,
+    padding: 12, // Réduction du padding
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 14, // Réduction de la taille de la police
+  },
+  errorMessage: {
+    color: 'red',
+    textAlign: 'center',
+    marginBottom: 10,
+  }
+});
+
+export default LoginScreen;
