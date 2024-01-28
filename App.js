906f891990b1eedda6d26c8c60b27596177ca6df
Naus4816 <128583047+Naus4816@users.noreply.github.com>
2024-01-28 16:01:27 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,143 +1,145 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { StyleSheet, View, Button, Image, TouchableOpacity } from 'react-native';
+import AsyncStorage  from '@react-native-async-storage/async-storage';
+
+//Screens
 import Login from './screens/Login';
+import UserList from './screens/UserList';
 import Welcome from './screens/Welcome';
 import Register from './screens/Register';
 import AdminDashboard from './screens/AdminDashboard';
 import UserDashboard from './screens/UserDashboard';
 import CountryDetails from './screens/CountryDetails';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import ConfirmPassword from './screens/ConfirmPassword';
+import ForgotPassword from './screens/ForgotPassword';
+import ResetPassword from './screens/ResetPassword';
+import EmailVerification from './screens/EmailVerification';
+import EditUserScreen from './screens/EditUser';
+import AddUserScreen from './screens/AddUser';
+import RecommandationListScreen from './screens/RecommandationList';
+import AddRecommandationScreen from './screens/AddRecommandation';
+import EditRecommandationScreen from './screens/EditRecommandation';
+import DeplacementsListScreen from './screens/DeplacementsList';
+import ActesSanteListScreen from './screens/ActesSanteList';
+import PaysListScreen from './screens/PaysList';
+import EditDeplacementsScreen from './screens/EditDeplacements';
+import EditActesSanteScreen from './screens/EditActesSante';
+import AddDeplacementsScreen from './screens/AddDeplacements';
+import AddActesSanteScreen from './screens/AddActesSante';
+import EditPaysScreen from './screens/EditPays';
+import AddPaysScreen from './screens/AddPays';
 
 const Stack = createNativeStackNavigator();
-const handleLogout = async () => {
-  await removeUserId();
-
-  navigation.navigate('Welcome');
-};
-
 
 const App = () => {
+
+  const LogoHeader = ({ navigation }) => (
+    <TouchableOpacity onPress={() => navigation.navigate('Welcome')}>
+      <Image 
+        source={require('./assets/logo.png')}
+        style={styles.logo}
+      />
+    </TouchableOpacity>
+  );
+
+  const AdminHeaderButtons = ({ navigation }) => (
+    <View style={styles.rightHeaderButtons}>
+      <Button title="AdminDashboard" onPress={() => navigation.navigate('AdminDashboard')} />
+      <Button title="Déconnexion" onPress={() => handleLogout(navigation)} />
+    </View>
+  );
+
+  const UserHeaderButtons = ({ navigation }) => (
+    <View style={styles.rightHeaderButtons}>
+      <Button title="UserDashboard" onPress={() => navigation.navigate('UserDashboard')} />
+      <Button title="Déconnexion" onPress={() => handleLogout(navigation)}/>
+    </View>
+  );
+  
+  const UnauthenticatedHeaderButtons = ({ navigation }) => (
+    <View style={styles.rightHeaderButtons}>
+      <Button title="Connexion" onPress={() => navigation.navigate('Login')} />
+      <Button title="Inscription" onPress={() => navigation.navigate('Register')} />
+    </View>
+  );
+
+
+  const handleLogout = async (navigation) => {
+    try {
+      await AsyncStorage.removeItem('VotreCléDeSession');
+      navigation.navigate('Welcome');
+    } catch (error) {
+      console.error('Erreur lors de la déconnexion:', error);
+    }
+  };
+
+  function headerUnauthenticated(navigation) {
+    return {
+      headerTitle: () => <LogoHeader navigation={navigation} />,
+      headerRight: () => <UnauthenticatedHeaderButtons navigation={navigation} />
+    };
+  }
+
+  function headerUser(navigation) {
+    return {
+      headerTitle: () => <LogoHeader navigation={navigation} />,
+      headerRight: () => <UserHeaderButtons navigation={navigation} />
+    };
+  }
+
+  function headerAdmin(navigation) {
+    return {
+      headerTitle: () => <LogoHeader navigation={navigation} />,
+      headerRight: () => <AdminHeaderButtons navigation={navigation} />
+    };
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-      <Stack.Screen 
-          name="Login" 
-          component={Login} 
-          options={({ navigation }) => ({
-            title: 'Sante-APP',
-            headerStyle: {
-              backgroundColor: '#ffffff', 
-            },
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
-            headerLeft: null,
-            headerRight: () => (
-              <View style={styles.headerSideButtons}>
-                <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-                  <Text style={styles.headerButton}>Login</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigation.navigate('Register')}>
-                  <Text style={styles.headerButton}>Register</Text>
-                </TouchableOpacity>
-              </View>
-            ),
-          })}
-        />
-        <Stack.Screen 
-          name="Welcome" 
-          component={Welcome} 
-          options={({ navigation }) => ({
-            title: 'Sante-APP',
-            headerStyle: {
-              backgroundColor: '#ffffff', 
-            },
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
-            headerLeft: null,
-            headerRight: () => (
-              <View style={styles.headerSideButtons}>
-                <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-                  <Text style={styles.headerButton}>Login</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigation.navigate('Register')}>
-                  <Text style={styles.headerButton}>Register</Text>
-                </TouchableOpacity>
-              </View>
-            ),
-          })}
-        />
-        <Stack.Screen
-          name="AdminDashboard"
-          component={AdminDashboard}
-            options={() => ({
-              headerRight: () => (
-                <View>
-                  <TouchableOpacity  onPress={handleLogout} >
-                    <Text style={styles.headerButton}>Déconnexion</Text>
-                  </TouchableOpacity>
-                </View>
-              ),
-            })}
-        />
-        <Stack.Screen
-          name="UserDashboard"
-          component={UserDashboard}
-            options={() => ({
-              headerRight: () => (
-                <View>
-                  <TouchableOpacity  onPress={handleLogout} >
-                    <Text style={styles.headerButton}>Déconnexion</Text>
-                  </TouchableOpacity>
-                </View>
-              ),
-            })}
-        />
-        <Stack.Screen
-          name="Register"
-          component={Register}
-            options={() => ({
-              headerRight: () => (
-                <View>
-                  <TouchableOpacity  onPress={handleLogout} >
-                    <Text style={styles.headerButton}>Déconnexion</Text>
-                  </TouchableOpacity>
-                </View>
-              ),
-            })}
-        />
-        <Stack.Screen
-          name="CountryDetails"
-          component={CountryDetails}
-          options={() => ({
-              headerRight: () => (
-                <View>
-                  <TouchableOpacity  onPress={handleLogout} >
-                    <Text style={styles.headerButton}>Déconnexion</Text>
-                  </TouchableOpacity>
-                </View>
-              ),
-            })}
-        />
+        <Stack.Screen name="Welcome" component={Welcome}  options={({ navigation }) => headerUnauthenticated(navigation)} />
+        <Stack.Screen name="ActesSanteListScreen" component={ActesSanteListScreen} options={({ navigation }) => headerAdmin(navigation)}/>
+        <Stack.Screen name="DeplacementsList" component={DeplacementsListScreen} options={({ navigation }) => headerAdmin(navigation)}/>
+        <Stack.Screen name="Login" component={Login} options={({ navigation }) => headerUnauthenticated(navigation)}/>
+        <Stack.Screen name="Register" component={Register} options={({ navigation }) => headerUnauthenticated(navigation)}/>
+        <Stack.Screen name="UserDashboard" component={UserDashboard}  options={({ navigation }) => headerUser(navigation)} />
+        <Stack.Screen name="CountryDetails" component={CountryDetails}  options={({ navigation }) => headerUser(navigation)} />
+        <Stack.Screen name="EmailVerification" component={EmailVerification} options={({ navigation }) => headerUnauthenticated(navigation)} />
+        <Stack.Screen name="ConfirmPassword" component={ConfirmPassword} options={({ navigation }) => headerUnauthenticated(navigation)}/>
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} options={({ navigation }) => headerUnauthenticated(navigation)}/>
+        <Stack.Screen name="ResetPassword" component={ResetPassword} options={({ navigation }) => headerUnauthenticated(navigation)}/>
+        <Stack.Screen name="AdminDashboard" component={AdminDashboard} options={({ navigation }) => headerAdmin(navigation)}/>
+        <Stack.Screen name="UserList" component={UserList} options={({ navigation }) => headerAdmin(navigation)}/>
+        <Stack.Screen name="AddUser" component={AddUserScreen} options={({ navigation }) => headerAdmin(navigation)}/>
+        <Stack.Screen name="EditUser" component={EditUserScreen} options={({ navigation }) => headerAdmin(navigation)}/>
+        <Stack.Screen name="RecommandationList" component={RecommandationListScreen} options={({ navigation }) => headerAdmin(navigation)}/>
+        <Stack.Screen name="AddRecommandation" component={AddRecommandationScreen} options={({ navigation }) => headerAdmin(navigation)}/>
+        <Stack.Screen name="EditRecommandation" component={EditRecommandationScreen} options={({ navigation }) => headerAdmin(navigation)}/>
+        <Stack.Screen name="PaysList" component={PaysListScreen} options={({ navigation }) => headerAdmin(navigation)}/>
+        <Stack.Screen name="EditDeplacements" component={EditDeplacementsScreen} options={({ navigation }) => headerAdmin(navigation)}/>
+        <Stack.Screen name="EditActesSante" component={EditActesSanteScreen} options={({ navigation }) => headerAdmin(navigation)}/>
+        <Stack.Screen name="AddDeplacements" component={AddDeplacementsScreen} options={({ navigation }) => headerAdmin(navigation)}/>
+        <Stack.Screen name="AddActesSante" component={AddActesSanteScreen} options={({ navigation }) => headerAdmin(navigation)}/>
+        <Stack.Screen name="EditPays" component={EditPaysScreen} options={({ navigation }) => headerAdmin(navigation)}/>
+        <Stack.Screen name="AddPays" component={AddPaysScreen} options={({ navigation }) => headerAdmin(navigation)}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
 const styles = StyleSheet.create({
-  headerSideButtons: {
+  logo: {
+    width: 100,
+    height: 40,
+    resizeMode: 'contain'
+  },
+  rightHeaderButtons: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: 120, 
-    marginRight: 100,
-  },
-  headerButton: {
-    color: '#000',
-    paddingHorizontal: 10, 
-    fontSize: 18, 
-  },
+    marginRight: 10,
+  }
 });
 
 export default App;
